e9431942edaa819b48292e09bb65d10b866ae4a9
momena <momena.salloum59@gmail.com>
2023-09-27 20:52:06 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+// Write your code here multiply
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+console.log(multiply)
 
-// Write your code here
+// generates a random integer greater than 0
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+// calculates a remainder
+let num3= 16
+let num4= 6
+let mod = num3 % num4
+console.log(mod)
+
+// the highest number in the set
+let max= Math.max(1,2,10,20)
+console.log(max)
+
+
+
+
+
